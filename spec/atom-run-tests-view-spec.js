c05e9bb82c1851447494dd25181b93e1ab7342c6
Wesley Luk <wesleyluk@gmail.com>
2017-08-12 15:28:14 -0700

    Initial commit

---
+++ spec/atom-run-tests-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRunTestsView from '../lib/atom-run-tests-view';
+
+describe('AtomRunTestsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
